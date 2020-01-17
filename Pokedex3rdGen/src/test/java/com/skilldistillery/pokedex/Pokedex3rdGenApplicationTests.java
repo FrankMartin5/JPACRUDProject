75e28e46e9b454a411470e996a9492dc960378ae
Frank Martin <cedricmrtn@gmail.com>
2020-01-17 15:14:23 -0700

    Connected JPA and SpringBoot project

---
+++ Pokedex3rdGen/src/test/java/com/skilldistillery/pokedex/Pokedex3rdGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pokedex3rdGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
