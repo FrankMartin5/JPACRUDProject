9a24bd4de7f8a08c56323baa3d0ad13234a35aaf
Frank Martin <cedricmrtn@gmail.com>
2020-01-20 09:08:34 -0700

    MVP

--- Pokedex3rdGen/src/main/java/com/skilldistillery/pokedex/Pokedex3rdGenApplication.java
+++ Pokedex3rdGen/src/main/java/com/skilldistillery/pokedex/Pokedex3rdGenApplication.java
@@ -2,12 +2,16 @@ package com.skilldistillery.pokedex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Pokedex3rdGenApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Pokedex3rdGenApplication.class, args);
-	}
-
+public class Pokedex3rdGenApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Pokedex3rdGenApplication.class);
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(Pokedex3rdGenApplication.class, args);
+  }
 }
